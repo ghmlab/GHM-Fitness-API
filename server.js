3898a9ba1b9e2eadf99661fe2090219816227e9c
Reza Hussain <alirezaa08@gmail.com>
2022-12-17 17:43:56 +0530

    Added index.html for Netlify Deploy

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const routes = require('./routes/routes.js')
+const routes = require('./src/routes/routes.js')
 
 const app = express()
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 
 app.use(bodyParser.json());
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
 app.use('/', routes)
 
-app.listen(PORT, () => console.log('App listening on port 9090'))
+app.listen(PORT, () => console.log('App listening on port 3000'))
